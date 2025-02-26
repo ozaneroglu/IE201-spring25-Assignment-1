6b6c23e50f1ba05db310edddc39706b26de5c730
Ozan Eroğlu <73275276+ozaneroglu@users.noreply.github.com>
2025-02-26 13:53:54 +0300

    Inital Commit

---
+++ main_test.py
@@ -0,0 +1,12 @@
+import pytest
+from main import int_to_roman
+
+#do not change this file
+
+def test_int_to_roman():
+    assert int_to_roman(3) == "III"
+    assert int_to_roman(9) == "IX"
+    assert int_to_roman(58) == "LVIII"
+    assert int_to_roman(400) == "CD"
+    assert int_to_roman(1994) == "MCMXCIV"
+    assert int_to_roman(3999) == "MMMCMXCIX"
