882a8ceb3160f02bbd0ac2706832e52df730820f
ozaneroglu <ozan.eroglu@boun.edu.tr>
2025-02-27 12:24:07 +0300

    Test Updated

--- main_test.py
+++ main_test.py
@@ -3,10 +3,19 @@ from main import int_to_roman
 
 # do not change this file
 
-def test_int_to_roman():
+def test_int_to_roman_3():
     assert int_to_roman(3) == "III"
-    assert int_to_roman(9) == "IX"
+
+
+def test_int_to_roman_58():
     assert int_to_roman(58) == "LVIII"
+
+def test_int_to_roman_400():
     assert int_to_roman(400) == "CD"
+
+def test_int_to_roman_1994():
     assert int_to_roman(1994) == "MCMXCIV"
+
+def test_int_to_roman_3999():
     assert int_to_roman(3999) == "MMMCMXCIX"
+
