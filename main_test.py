223812433705728902b5d7a70fe6bb297985d784
ozaneroglu <ozan.eroglu@boun.edu.tr>
2025-02-27 12:49:06 +0300

    Test Functions Seperated

--- main_test.py
+++ main_test.py
@@ -7,15 +7,4 @@ def test_int_to_roman_3():
     assert int_to_roman(3) == "III"
 
 
-def test_int_to_roman_58():
-    assert int_to_roman(58) == "LVIII"
-
-def test_int_to_roman_400():
-    assert int_to_roman(400) == "CD"
-
-def test_int_to_roman_1994():
-    assert int_to_roman(1994) == "MCMXCIV"
-
-def test_int_to_roman_3999():
-    assert int_to_roman(3999) == "MMMCMXCIX"
 
