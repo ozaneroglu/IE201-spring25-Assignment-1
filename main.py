90720bff614dd621e791431737b9040cb07dc5e3
Ozan Eroğlu <73275276+ozaneroglu@users.noreply.github.com>
2025-02-27 12:27:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ def int_to_roman(num):
     Convert an integer to a Roman numeral.
 
     :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer, make sure symbols are uppercase.
+    :return: A string representing the Roman numeral of the integer.
     """
